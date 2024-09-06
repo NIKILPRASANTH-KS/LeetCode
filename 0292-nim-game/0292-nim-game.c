177cef0c91a9c1157ae9ae9557c99dabfdbe551e
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-09-06 16:22:35 +0530

    Time: 0 ms (100%), Space: 7.6 MB (44.11%) - LeetHub

--- 0292-nim-game/0292-nim-game.c
+++ 0292-nim-game/0292-nim-game.c
@@ -2,7 +2,7 @@ bool canWinNim(int n) {
     if(n>=1 && n<=3) return true;
     if (n%4==0) return false;
     if(n%2==0 && n%3==0) return true; 
-    if(n%5==0 || n%2==1 ||n%7==0) return true;
+    if(n%5==0 || n%7==0 || n%2==1) return true;
     while(n%2==0){
         n/=2;
         if(n%2!=0) return true;
