88a80018e6168221208eea9ad4d4d5a20e2ad173
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-09-07 15:57:14 +0530

    Time: 3 ms (85.37%), Space: 8.3 MB (44.95%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.c
@@ -0,0 +1,9 @@
+int arraySign(int* arr, int n) {
+    int pc=0,nc=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0) return 0;
+        if(arr[i]<0) nc++;
+    }
+    if(nc%2!=0) return -1;
+    return 1;
+}
