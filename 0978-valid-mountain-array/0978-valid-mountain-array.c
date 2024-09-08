8828da9a324768d378dc8897301f53b275804946
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-09-08 13:38:38 +0530

    Time: 21 ms (42.31%), Space: 9.1 MB (43.75%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.c
+++ 0978-valid-mountain-array/0978-valid-mountain-array.c
@@ -1,12 +1,12 @@
 bool validMountainArray(int* arr, int n){
-    if (n<=2) return false;
-    int mark,flag=0;
+    if(n<3) return false;
     if (arr[0]>arr[1]) return false;
+    int mark=0,flag=0;
     for(int i=0;i<n-1;i++){
-        if(arr[i+1]>arr[i]){
+        if(arr[i]<arr[i+1]){
             mark=i+1;
         }
-        else if(arr[i+1]==arr[i]) return false;
+        else if(arr[i]==arr[i+1]) return false;
         else{
             flag=1;
             break;
@@ -14,11 +14,33 @@ bool validMountainArray(int* arr, int n){
     }
     int i=mark;
     while(i<n-1){
-        if(!(arr[i+1]<arr[i])){
-            return false;
-        }
+        if(arr[i]<=arr[i+1]) return false;
         i++;
     }
-    if (flag==1) return true;
-    return false;
+    if(flag==0) return false;
+    return true;
 }
+// bool validMountainArray(int* arr, int n){
+//     if (n<=2) return false;
+//     int mark,flag=0;
+//     if (arr[0]>arr[1]) return false;
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i+1]>arr[i]){
+//             mark=i+1;
+//         }
+//         else if(arr[i+1]==arr[i]) return false;
+//         else{
+//             flag=1;
+//             break;
+//         }
+//     }
+//     int i=mark;
+//     while(i<n-1){
+//         if(!(arr[i+1]<arr[i])){
+//             return false;
+//         }
+//         i++;
+//     }
+//     if (flag==1) return true;
+//     return false;
+// }
