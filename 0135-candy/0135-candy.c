4af469cea9cf3326326381376454bc1ca7c9f4a7
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-09-11 09:27:20 +0530

    Time: 9 ms (97.74%), Space: 9.7 MB (13.35%) - LeetHub

--- 0135-candy/0135-candy.c
+++ 0135-candy/0135-candy.c
@@ -1,18 +1,56 @@
 int candy(int* arr, int n) {
-    int max=0;
-    int* a=(int*)malloc(n*sizeof(int));
+    int* ans=(int*)malloc(n*sizeof(int));
     for(int i=0;i<n;i++){
-        a[i]=1;
+        ans[i]=1;
     }
     for(int i=1;i<n;i++){
-        if(arr[i]>arr[i-1]) a[i]=a[i-1]+1;
+        if(arr[i]>arr[i-1]) ans[i]=ans[i-1]+1;
     }
     for(int i=n-2;i>=0;i--){
-        if(arr[i]>arr[i+1] && a[i]<=a[i+1]) a[i]=a[i+1]+1;
+        if(arr[i]>arr[i+1] && ans[i]<=ans[i+1]) ans[i]=ans[i+1]+1;
     }
     int sum=0;
     for(int i=0;i<n;i++){
-        sum+=a[i];
+        sum+=ans[i];
     }
     return sum;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int candy(int* arr, int n) {
+//     int max=0;
+//     int* a=(int*)malloc(n*sizeof(int));
+//     for(int i=0;i<n;i++){
+//         a[i]=1;
+//     }
+//     for(int i=1;i<n;i++){
+//         if(arr[i]>arr[i-1]) a[i]=a[i-1]+1;
+//     }
+//     for(int i=n-2;i>=0;i--){
+//         if(arr[i]>arr[i+1] && a[i]<=a[i+1]) a[i]=a[i+1]+1;
+//     }
+//     int sum=0;
+//     for(int i=0;i<n;i++){
+//         sum+=a[i];
+//     }
+//     return sum;
+// }
