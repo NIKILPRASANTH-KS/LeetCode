7c59929aa3380d41467d2cade02bc4b7f309ac70
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-09-24 10:15:59 +0530

    Time: 9 ms (9.6%), Space: 7.8 MB (71.92%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.c
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -4,12 +4,10 @@ int compare(const void* a,const void* b){
 char findTheDifference(char* s, char* t) {
     int lent=strlen(t);
     int lens=strlen(s);
-    int temp=-1;
     qsort(s,lens,sizeof(char),compare);
     qsort(t,lent,sizeof(char),compare);
     for(int i=0;i<lent;i++){
        if(s[i]!=t[i]) return t[i];
     }
-    if(temp==-1) temp=lent;
-    return t[temp];
+    return t[lent];
 }
