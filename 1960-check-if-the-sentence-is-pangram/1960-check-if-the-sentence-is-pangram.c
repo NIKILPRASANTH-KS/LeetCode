eb8141f5124732986eac25cef685b591b3930f98
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-10-09 06:48:20 +0530

    Time: 3 ms (52.32%), Space: 7.7 MB (69.2%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.c
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.c
@@ -9,19 +9,3 @@ bool checkIfPangram(char* s) {
     }
     return true;
 }
-// bool isAnagram(char* s, char* t) {
-//     int* arr=(int*)calloc(26,sizeof(int));
-//     int lens=strlen(s);
-//     int lent=strlen(t);
-//     if(lens!=lent) return false;
-//     for(int i=0;i<lens;i++){ 
-//         arr[s[i]-'a']++;     
-//     }
-//     for(int i=0;i<lent;i++){
-//         arr[t[i]-'a']--;     
-//     }
-//     for(int i=0;i<26;i++){
-//         if(arr[i]!=0) return false;
-//     }
-//     return true;
-// }
