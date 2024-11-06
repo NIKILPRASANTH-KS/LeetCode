a3e1b1109afbd07ebaaa9ca54c39bd1c0610950e
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2024-11-06 11:15:42 +0530

    Time: 4 ms (61.54%), Space: 9.2 MB (10.64%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.c
+++ 3463-alternating-groups-i/3463-alternating-groups-i.c
@@ -3,9 +3,7 @@ int numberOfAlternatingGroups(int* colors, int c) {
     for(int i=1;i<c-1;i++){
         if(colors[i-1]!=colors[i] && colors[i+1]!=colors[i]) count++;
     }
-    if(c>=3){
-        if(colors[c-1]!=colors[c-2] && colors[c-1]!=colors[0]) count++;
-        if(colors[c-1]!=colors[0] && colors[1]!=colors[0]) count++;
-    }
+    if(colors[c-1]!=colors[c-2] && colors[c-1]!=colors[0]) count++;
+    if(colors[c-1]!=colors[0] && colors[1]!=colors[0]) count++;
     return count;
 }
