2a3ac595bcb91e5a55ef7ef48d6786ecdcf19b9d
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2025-01-08 10:17:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (64.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.c
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.c
@@ -1,7 +1,19 @@
 int singleNonDuplicate(int* nums, int n) {
-    int result=0;
-    for(int i=0;i<n;i++){
-        result^=nums[i];
+    int start=0,end=n-1;
+    while(start<end){
+        int mid=start+(end-start)/2;
+        if(mid%2==1) mid--;
+        if(nums[mid]==nums[mid+1]) start=mid+2;
+        else end=mid;
     }
-    return result;
+
+    return nums[start];
 }
+
+// int singleNonDuplicate(int* nums, int n) {
+//     int result=0;
+//     for(int i=0;i<n;i++){
+//         result^=nums[i];
+//     }
+//     return result;
+// }
