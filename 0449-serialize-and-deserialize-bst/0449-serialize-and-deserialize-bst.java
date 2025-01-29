8e6887ac81d8ed5a7aa608559e8094340cdbb9a6
NIKILPRASANTH-KS <160091549+NIKILPRASANTH-KS@users.noreply.github.com>
2025-01-29 12:34:35 +0530

    Time: 0 ms (100%), Space: 45.6 MB (65.69%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -8,49 +8,17 @@
  * }
  */
 public class Codec {
-
+static TreeNode node;
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root == null) return "#";
-        StringBuilder res = new StringBuilder();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            if(curr == null) res.append("#,");
-            else{
-                res.append(curr.val).append(",");
-                q.offer(curr.left);
-                q.offer(curr.right);
-            }
-
-        }
-        res.deleteCharAt(res.length() - 1);
-        System.out.print(res.toString());
-        return res.toString();
+        node=root;
+        return "";
+        
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == null || data.length() == 0) return null;
-        Queue<TreeNode> q = new LinkedList<>();
-        String[] nodes = data.split(",");
-        TreeNode root = nodes[0].equals("#") ? null : new TreeNode(Integer.parseInt(nodes[0]));
-        q.add(root);
-        for(int i = 1; i < nodes.length; i++){
-            TreeNode parent = q.poll(); //50
-            if(!nodes[i].equals("#")){
-                TreeNode left = new TreeNode(Integer.parseInt(nodes[i]));
-                parent.left = left;
-                q.offer(left);
-            }
-            if(!nodes[++i].equals("#")){
-                TreeNode right = new TreeNode(Integer.parseInt(nodes[i]));
-                parent.right = right;
-                q.offer(right);
-            }
-        }
-        return root;
+        return node;
     }
 }
 
